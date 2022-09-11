fbc207b595e9865f982a6a3015d803edbe8ef5ed
yangwenbin <1044209998@qq.com>
2022-09-11 22:48:40 +0800

    logs 基础

---
+++ lengo2/config/config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/astaxie/beego/config"
+)
+
+func main() {
+	conf, err := config.NewConfig("ini", "./logcollect.conf")
+	if err != nil {
+		fmt.Println("new config failed, err:", err)
+		return
+	}
+
+	port, err := conf.Int("server::listen_port")
+	if err != nil {
+		fmt.Println("read server:port failed, err:", err)
+		return
+	}
+
+	fmt.Println("Port:", port)
+	log_level := conf.String("logs::log_level")
+	if err != nil {
+		fmt.Println("read log_level failed, ", err)
+		return
+	}
+	fmt.Println("logs:", log_level)
+
+	log_path := conf.String("logs::log_path")
+	fmt.Println("log_path:", log_path)
+}
